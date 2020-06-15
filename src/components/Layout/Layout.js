813dcec12e552047e58cdf636aedfbc7f0b3fbc6
Gabi <csgabka@gmail.com>
2020-06-15 15:13:48 +0100

    sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxilliary from '../../hoc/Auxilliary/Auxilliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
-const layout = ( props ) => {
-  return (
-    <Auxilliary>
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+  }
+
+  render() {
+    return (
+      <Auxilliary>
       <div>A lot TODO still</div>
       <Toolbar />
+      <SideDrawer open={this.state.showSideDrawer}
+      closed={this.sideDrawerClosedHandler} />
       <main className={classes.Content}>
-        {props.children}
+        {this.props.children}
       </main>
     </Auxilliary>
-  );
-
+  )
+}
 }
 
-export default layout;
+export default Layout;
